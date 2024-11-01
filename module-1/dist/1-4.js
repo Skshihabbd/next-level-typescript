d40b26a218da1f0791fa58891b98e16ac7ce8cf2
unknown <skshihab170@gmail.com>
2024-11-01 11:27:02 +0600

    readonly

---
+++ module-1/dist/1-4.js
@@ -0,0 +1,24 @@
+"use strict";
+// string type data
+var names = "shihab";
+// number type data
+var age = 27;
+// bolean type data
+var isTrue = true;
+// undefined
+var x = undefined;
+// null
+var y = null;
+// anytype
+var d;
+d = 12;
+d = true;
+d = "string";
+// array type data
+var friends = ["bidyut", "sobuj"];
+var rollList = [1, 3];
+rollList.push(6);
+// tuple type data
+var coordinates = [1, 5];
+var ageName = [50, "raihan", true];
+ageName[1] = "any";
